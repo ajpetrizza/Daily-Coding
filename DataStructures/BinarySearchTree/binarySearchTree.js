4f094b90e8211cf8c540129be9c4212b03ba13c2
ajpetrizza <anthony.petrizza@gmail.com>
2024-02-08 15:20:07 -0700

    DFS & BFS Tree traversal

--- DataStructures/BinarySearchTree/binarySearchTree.js
+++ DataStructures/BinarySearchTree/binarySearchTree.js
@@ -50,4 +50,59 @@ class BinarySearchTree {
 		if (!found) return undefined;
 		return current;
 	}
+	BFS() {
+		let data = [];
+		let queue = [];
+		let node = this.root;
+		queue.push(node);
+		while (queue.length) {
+			node = queue.shift();
+			data.push(node); // .value would allow you to see number directly
+			if (node.left) queue.push(node.left);
+			if (node.right) queue.push(node.right);
+		}
+		return data;
+	}
+	DFSPreOrder() {
+		let data = [];
+		let current = this.root;
+		function traverse(node) {
+			data.push(node.value);
+			if (node.left) traverse(node.left);
+			if (node.right) traverse(node.right);
+		}
+		traverse(current);
+		return data;
+	}
+	DFSPostOrder() {
+		let data = [];
+		let current = this.root;
+		function traverse(node) {
+			if (node.left) traverse(node.left);
+			if (node.right) traverse(node.right);
+			data.push(node.value);
+		}
+		traverse(current);
+		return data;
+	}
+	DFSInOrder() {
+		let data = [];
+		let current = this.root;
+		function traverse(node) {
+			if (node.left) traverse(node.left);
+			data.push(node.value);
+			if (node.right) traverse(node.right);
+		}
+		traverse(current);
+		return data;
+	}
 }
+
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(6);
+tree.insert(15);
+tree.insert(3);
+tree.insert(8);
+tree.insert(20);
+console.log(tree.DFSInOrder());
