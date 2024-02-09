558ff673cb9e1a18e3b52a8fdbcc27778b8ec6f1
ajpetrizza <anthony.petrizza@gmail.com>
2024-02-09 15:17:53 -0700

    Binary heap

--- DataStructures/BinaryHeaps/maxBinaryHeap.js
+++ DataStructures/BinaryHeaps/maxBinaryHeap.js
@@ -16,13 +16,58 @@ class MaxBinaryHeap {
 		}
 		return this;
 	}
+	extractMax() {
+		// swap first and last
+		let max = this.values[0];
+		let end = this.values.pop();
+		if (this.values.length > 0) {
+			this.values[0] = end;
+			// SINK DOWN
+			this.sinkDown();
+		}
+		return max;
+	}
+	sinkDown() {
+		let idx = 0;
+		const length = this.values.length;
+		const element = this.values[0];
+		while (true) {
+			let leftChildIndex = 2 * idx + 1;
+			let rightChildIndex = 2 * idx + 2;
+			let leftChild, rightChild;
+			let swap = null;
+			if (leftChildIndex < length) {
+				leftChild = this.values[leftChildIndex];
+				if (leftChild > element) {
+					swap = leftChildIndex;
+				}
+			}
+			if (rightChildIndex < length) {
+				rightChild = this.values[rightChildIndex];
+				if (
+					(swap === null && rightChild > element) ||
+					(swap !== null && rightChild > leftChild)
+				) {
+					swap = rightChildIndex;
+				}
+			}
+			if (swap === null) break;
+			// if swap has a value, now swap those values
+			this.values[idx] = this.values[swap];
+			this.values[swap] = element;
+			// now swap the index with the index of the element you swapped with
+			idx = swap;
+		}
+	}
 }
 
 let maxHeap = new MaxBinaryHeap();
 maxHeap.insert(41);
 maxHeap.insert(39);
+maxHeap.insert(33);
 maxHeap.insert(18);
 maxHeap.insert(27);
 maxHeap.insert(12);
-maxHeap.insert(33);
-console.log(maxHeap.insert(55));
+maxHeap.insert(55);
+console.log(maxHeap.extractMax());
+console.log(maxHeap.values);
